8d9c9702e329116b2e599713fd1667b6da6cdb5a
Hygmod <joshsdanton@gmail.com>
2022-08-29 22:21:33 -0700

    modified css

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -4,11 +4,21 @@ let gridSize = 20
 
 export default function Grid() {
     let grid = []
+    let word = 'word'
 
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const id='row' + i + '_' + 'col' +j
-            grid.push(<div className="gridTile" id={id}>{i},{j}</div>)
+    let startingPosition = [5, 5]
+
+    for (let i = 1; i <= gridSize; i++) {
+        for (let j = 1; j <= gridSize; j++) {
+            const id = 'row' + i + '_' + 'col' + j
+            grid.push(<div className="gridTile" id={id}>
+
+                {(i===startingPosition[0]&&j===startingPosition[1]) ? <p>{word[0]}</p> : <></>}
+                {(i===startingPosition[0]&&j===startingPosition[1]+1) ? <p>{word[1]}</p> : <></>}
+                {(i===startingPosition[0]&&j===startingPosition[1]+2) ? <p>{word[2]}</p> : <></>}
+                {(i===startingPosition[0]&&j===startingPosition[1]+3) ? <p>{word[3]}</p> : <></>}
+
+            </div>)
         }
     }
 
