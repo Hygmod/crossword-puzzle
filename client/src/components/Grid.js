21e00fef7671cc4cbb05d909c51ba31f4134600c
Hygmod <joshsdanton@gmail.com>
2022-08-31 21:59:51 -0700

    updated Tile component

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,22 +1,19 @@
 import React from 'react'
+import Tile from './Tile'
 
 let gridSize = 20
 
 export default function Grid() {
     let grid = []
     let word = 'word'
+    let gridSize = 20
 
     let startingPosition = [5, 5]
 
     for (let i = 1; i <= gridSize; i++) {
         for (let j = 1; j <= gridSize; j++) {
             const id = 'row' + i + '_' + 'col' + j
-            grid.push(<div className="Tile" id={id}>
-                {(i === startingPosition[0] && j === startingPosition[1]) ? <p>{word[0]}</p> : <></>}
-                {(i === startingPosition[0] && j === startingPosition[1] + 1) ? <p>{word[1]}</p> : <></>}
-                {(i === startingPosition[0] && j === startingPosition[1] + 2) ? <p>{word[2]}</p> : <></>}
-                {(i === startingPosition[0] && j === startingPosition[1] + 3) ? <p>{word[3]}</p> : <></>}
-            </div>)
+            grid.push(<Tile gridSize id={id} word={word}/>)
         }
     }
 
