0cda09a40adcfdfbeac337aea5421676ccde4bc6
Hygmod <joshsdanton@gmail.com>
2022-09-04 20:24:43 -0700

    updated Tile, Grid components

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,43 +1,34 @@
-import React from 'react'
-import Tile from './Tile'
+import React from "react";
+import Tile from "./Tile";
 
-let gridSize = 20
+let gridSize = 20;
 
 export default function Grid() {
-    let grid = []
-    let words = [
-        {
-            word: 'bark',
-            positions: [[1, 3], [1, 4], [1, 5], [1, 6]]
-        },
-        {
-            word: 'bite',
-            positions: [[3, 3], [4, 3], [5, 3], [6, 4]]
-        }
-    ]
-    const mapPositions = words.map(e => e.positions)
-    const mapPositionsRows = mapPositions.map(e => e[0])
-    const mapPositionsColumns = mapPositions.map(e => e[1])
-    // console.log(mapPositions)
-    console.log(mapPositionsRows)
+  let grid = [];
+  let words = [
+    {
+      word: "bark",
+      startPosition: [1, 3],
+    },
+    {
+      word: "bite",
+      startPosition: [3, 3],
+    },
+  ];
+  const mapPositions = words.map((e) => e.startPosition);
+  const mapPositionsRows = mapPositions.map((e) => e[0]);
+  const mapPositionsColumns = mapPositions.map((e) => e[1]);
 
-    for (let i = 1; i <= gridSize; i++) {
-        for (let j = 1; j <= gridSize; j++) {
-            let counter = 0
-            const id = 'row' + i + '_' + 'col' + j
-            if (mapPositionsRows.includes(i) && mapPositionsColumns.includes(j)) {
-                const wordToFill = words.findIndex(e => (e.positions[0] === i) && (e.positions[1] === j))
-                grid.push(<Tile gridSize id={id} letter={words[wordToFill].word[0]} />)
-                if (counter < words[wordToFill].word.length) {
-                } else {
-                    counter = 0
-                }
-            } else {
-                grid.push(<Tile gridSize id={id} letter={'z'} />)
-            }
-
-        }
+  for (let i = 1; i <= gridSize; i++) {
+    for (let j = 1; j <= gridSize; j++) {
+      const id = `row${i}_col${j}`;
+      grid.push(<Tile gridSize id={id} key={id} letter="b" />);
     }
+  }
 
-    return <div id="grid">{grid}</div>
+  return (
+    <div id="grid" key="grid">
+      {grid}
+    </div>
+  );
 }
