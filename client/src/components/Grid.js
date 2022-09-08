2423e92bba5c24dd0a94f0ce3b832023fdfc4e20
Hygmod <joshsdanton@gmail.com>
2022-09-07 20:49:01 -0700

    Updated Grid component

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,36 +1,61 @@
 import React from "react"
 import Tile from "./Tile"
-import wordList from "../wordList"
+import importedWordList from "../wordList"
 
-const numberOfWordsToAdd = 10
+let wordList = importedWordList
+
+const numberOfWordsToAdd = 50
 
 let gridSize = 20
 
 export default function Grid() {
-  const wordListLength = wordList.length
+  let shortestWord = wordList.reduce((a, c) => (c.length <= a.length ? c : a)).length
+
+  let wordListLength = wordList.length
 
   const letterPositionsOnGrid = []
   let grid = []
-  let words = [
+  let words = []
 
-  ]
-  for (let i = 1; i <= numberOfWordsToAdd; i++) {
-    const randomWord = wordList[Math.floor(Math.random() * wordListLength)]
-    const minRandomNumber = 1
-    const maxRandomNumber = 15
-    const randomwRow = Math.floor(Math.random() * (maxRandomNumber - minRandomNumber) + minRandomNumber)
-    const randomColumn = Math.floor(Math.random() * (maxRandomNumber - minRandomNumber) + minRandomNumber)
-    const randomOrientation = Math.random() > 0.5
-
-    words.push({
-      word: randomWord,
-      startPosition: [randomwRow, randomColumn],
-      isHorizontal: randomOrientation,
-      positions: [],
-    })
+  //Check if word fits on grid
+  const checkToSeeIfTheWordWillFitOnTheGrid = function (w) {
+    if (w.isHorizontal) {
+      if (w.word.length + w.startPosition[1] > 20) {
+        return false
+      } else return true
+    } else {
+      if (w.word.length + w.startPosition[0] > 20) {
+        return false
+      } else return true
+    }
   }
 
-  words.forEach((w) => {
+  //Check if word overlaps or neighbors a same-orientation word
+  const checkIfWordOverlapsOrNeighborsASameOrientationWord = function (w) {
+    let y = w.isHorizontal ? 0 : 1
+    let x = w.isHorizontal ? 1 : 0
+    
+      const arr = []
+
+      for (let i = w.startPosition[x]; i < w.startPosition[x] + w.word.length; i++) {
+        arr.push(i)
+      }
+
+      const sharesColumns = letterPositionsOnGrid.map((e) => {
+        return arr.includes(e.position[x])
+      })
+
+      const hasSharedColumns = sharesColumns.includes(true)
+
+      const overlapsAnotherWord = letterPositionsOnGrid.filter((e) => {
+        return (e.position[y] === w.startPosition[y] || e.position[y] === w.startPosition[y] + 1 || e.position[y] === w.startPosition[y] - 1) && hasSharedColumns
+      })
+
+      return overlapsAnotherWord.length > 0
+    
+  }
+
+  const addLetterPostions = function (w) {
     const wordLength = w.word.length
 
     letterPositionsOnGrid.push({
@@ -53,7 +78,40 @@ export default function Grid() {
         })
       }
     }
-  })
+  }
+
+  for (let i = 1; i <= numberOfWordsToAdd; i++) {
+    const randomWord = wordList[Math.floor(Math.random() * wordListLength)]
+    const minRandomNumber = 1
+    const maxRandomNumber = gridSize - shortestWord
+    const randomwRow = Math.floor(Math.random() * (maxRandomNumber - minRandomNumber) + minRandomNumber)
+    const randomColumn = Math.floor(Math.random() * (maxRandomNumber - minRandomNumber) + minRandomNumber)
+    const randomOrientation = Math.random() > 0.5
+
+    const currentWord = {
+      word: randomWord,
+      startPosition: [randomwRow, randomColumn],
+      isHorizontal: randomOrientation,
+      // positions: [],
+    }
+
+    //Check if word fits on grid
+    const wordWillFit = checkToSeeIfTheWordWillFitOnTheGrid(currentWord)
+
+    //Check if word overlaps or neighbors a same-orientation word
+    const overlapsWithAnotherWord = checkIfWordOverlapsOrNeighborsASameOrientationWord(currentWord)
+    //Check if overlaps occur with different-orientation word, and if so, with same letter
+
+    if (wordWillFit && !overlapsWithAnotherWord) {
+      addLetterPostions(currentWord)
+      //   console.log(currentWord)
+      console.log(wordList)
+      wordList = wordList.filter(e=> e != currentWord.word)
+      wordListLength = wordList.length
+      console.log(wordList)
+      words.push(currentWord)
+    }
+  }
 
   for (let row = 1; row <= gridSize; row++) {
     for (let column = 1; column <= gridSize; column++) {
