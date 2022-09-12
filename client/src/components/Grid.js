99f9f4f2f697cf672574c6566c593a515400c4f5
Hygmod <joshsdanton@gmail.com>
2022-09-11 21:13:07 -0700

    updated Grid

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -4,12 +4,11 @@ import importedWordList from "../wordList"
 
 let wordList = importedWordList
 
-const numberOfWordsToAdd = 100
+const numberOfWordsToAdd = 5
 
 let gridSize = 20
 
 export default function Grid() {
-  // let shortestWord = wordList.reduce((a, c) => (c.length <= a.length ? c : a)).length
   const letterPositionsOnGrid = []
   let grid = []
   let words = []
@@ -27,34 +26,94 @@ export default function Grid() {
     }
   }
 
+  //Check for overlaps, new version
+  const checkForOverlaps = function (w) {
+    let arr = []
+
+    if (w.isHorizontal) {
+      arr = [w.startPosition[0] - 1, w.startPosition[0], w.startPosition[0] + 1]
+
+      console.log(words)
+      console.log(
+        words.filter((e) => {
+          e.positions
+            .map((el) => {
+              arr.includes(el[0])
+            })
+            .includes(true)
+        }).length > 0
+      )
+
+      const overlapsOnPrimaryAxis =
+        words.filter((e) => {
+          e.positions
+            .map((el) => {
+              arr.includes(el[0])
+            })
+            .includes(true)
+        }).length > 0
+
+      return overlapsOnPrimaryAxis
+    } else {
+      arr = [w.startPosition[1] - 1, w.startPosition[1], w.startPosition[1] + 1]
+
+      console.log(words)
+      console.log(
+        words.filter((e) => {
+          e.positions
+            .map((el) => {
+              arr.includes(el[1])
+            })
+            .includes(true)
+        }).length > 0
+      )
+
+      const overlapsOnPrimaryAxis =
+        words.filter((e) => {
+          e.positions
+            .map((el) => {
+              arr.includes(el[1])
+            })
+            .includes(true)
+        }).length > 0
+
+      return overlapsOnPrimaryAxis
+    }
+  }
+
   //Check if word overlaps or neighbors a same-orientation word
   const checkIfWordOverlapsOrNeighborsASameOrientationWord = function (w) {
-    let y = w.isHorizontal ? 0 : 1
     let x = w.isHorizontal ? 1 : 0
+    let y = w.isHorizontal ? 0 : 1
     const arr = []
-    const arr2 = [w.startPosition[y],w.startPosition[y]+1,w.startPosition[y]-1]
+    const arr2 = [w.startPosition[y] - 1, w.startPosition[y], w.startPosition[y] + 1]
     for (let i = w.startPosition[x]; i < w.startPosition[x] + w.word.length; i++) {
       arr.push(i)
     }
+
     const sharesOppositeOrientationPositions = letterPositionsOnGrid.map((e) => arr.includes(e.position[x]) && arr2.includes(e.position[y])).includes(true)
-    const overlapsAnotherWord = letterPositionsOnGrid.filter((e) => {
-      return sharesOppositeOrientationPositions
-    })
-    return overlapsAnotherWord.length > 0
+
+    return sharesOppositeOrientationPositions
   }
+
+  //Check if overlaps occur with different-orientation word, and if so, with same letter
+  const checkIfWordOverlapsOrNeighborsADifferentOrientationWord = function (w) {}
 
   const addLetterPostions = function (w) {
     const wordLength = w.word.length
+    let wordPositions = []
     letterPositionsOnGrid.push({
       position: w.startPosition,
       letter: w.word[0],
     })
+    wordPositions.push(w.startPosition)
     if (w.isHorizontal) {
       for (let i = 1; i < wordLength; i++) {
         letterPositionsOnGrid.push({
           position: [w.startPosition[0], w.startPosition[1] + i],
           letter: w.word[i],
         })
+        wordPositions.push([w.startPosition[0], w.startPosition[1] + i])
       }
     } else {
       for (let i = 1; i < wordLength; i++) {
@@ -62,16 +121,16 @@ export default function Grid() {
           position: [w.startPosition[0] + i, w.startPosition[1]],
           letter: w.word[i],
         })
+        wordPositions.push([w.startPosition[0] + i, w.startPosition[1]])
       }
     }
+    return wordPositions
   }
 
   const generateRandomNumberBetweenTwoBounds = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
-  let counterPre = 0
-  let counterPost = 0
   let counter = 0
-  const maxTriesToFitWord = 100
+  const maxTriesToFitWord = 3
 
   for (let i = 1; i <= numberOfWordsToAdd; i++) {
     const randomWord = wordList[Math.floor(Math.random() * wordList.length)]
@@ -83,33 +142,32 @@ export default function Grid() {
     let overlapsWithAnotherWord = true
 
     while (!wordWillFit && overlapsWithAnotherWord && counter < maxTriesToFitWord) {
-      const currentWord = {
+      let currentWord = {
         word: randomWord,
         startPosition: [generateRandomNumberBetweenTwoBounds(minStartingPosition, maxStartingPosition), generateRandomNumberBetweenTwoBounds(minStartingPosition, maxStartingPosition)],
         isHorizontal: randomOrientation,
         positions: [],
       }
-      counterPre++
 
       //Check if word fits on grid
       wordWillFit = checkToSeeIfTheWordWillFitOnTheGrid(currentWord)
 
       //Check if word overlaps or neighbors a same-orientation word
-      overlapsWithAnotherWord = checkIfWordOverlapsOrNeighborsASameOrientationWord(currentWord)
+      //overlapsWithAnotherWord = checkIfWordOverlapsOrNeighborsASameOrientationWord(currentWord)
+      console.log(currentWord)
+      overlapsWithAnotherWord = checkForOverlaps(currentWord)
 
       //Check if overlaps occur with different-orientation word, and if so, with same letter
 
-
-
+      //if (wordWillFit && !overlapsWithAnotherWord && wordList.length > 1) {
       if (wordWillFit && !overlapsWithAnotherWord && wordList.length > 1) {
-        addLetterPostions(currentWord)
+        const wordPositions = addLetterPostions(currentWord)
 
         wordList = wordList.filter((e) => e !== currentWord.word)
-        console.log(wordList.length)
+
+        currentWord.positions = wordPositions
 
         words.push(currentWord)
-
-        counterPost++
       } else {
         wordWillFit = false
         overlapsWithAnotherWord = true
@@ -119,12 +177,9 @@ export default function Grid() {
     counter = 0
   }
 
-  console.log(`counterPre = ${counterPre}`)
-  console.log(`counterPost = ${counterPost}`)
-
   for (let row = 1; row <= gridSize; row++) {
     for (let column = 1; column <= gridSize; column++) {
-      const id = `row${row}_col${column}`
+      const id = `${row}_${column}`
       const letter = letterPositionsOnGrid.filter((element) => element.position[0] === row && element.position[1] === column)
       grid.push(<Tile id={id} key={id} className={letter[0] ? "hasLetter" : ""} letter={letter[0] ? letter[0].letter : ""} />)
     }
--- client/src/components/Tile.js
+++ client/src/components/Tile.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from "react"
 
-export default function Grid(props) {
-    return <span className={`tile ${props.className}`} id={props.id} key={props.id}>{props.letter}</span>
+export default function Tile(props) {
+  return (
+    <>
+    <span className={`tile ${props.className}`} id={props.id} key={props.id}>
+     {props.letter}
+    </span>
+    </>
+   )
+  
 }
